bf52acbdaed2d455be1d18d18e9eddd90733c5da
Ivanna Navrotska <navrockaivanna15@gmail.com>
2024-05-25 19:29:06 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,20 +1,23 @@
 #here you can find gcd of any two integers
 
-def gcd (a,b):
+def gcd(a, b):
+    if not (isinstance(a, int) and isinstance(b, int)):
+        return None
 
     if a == 0 and b == 0:
-        return
+        return None
     elif b == 0:
         return a
     else:
-        return gcd(b, a%b)
+        return gcd(b, a % b)
 
 #in case you forgot the fibonacci numbers
 
-def fibonacci (f):
+def fibonacci(f):
+    if not isinstance(f, int) or f < 0:
+        return None
 
     if f == 0 or f == 1:
         return 1
     else:
-        return fibonacci(f-1) + fibonacci(f-2)
-     
+        return fibonacci(f - 1) + fibonacci(f - 2)
